e77b9c16ad60b9e53340229f0ba6c1265e7691b4
silviuilas <silviuilas@gmail.com>
2021-04-21 20:39:54 +0300

    Added some more tests

--- Tests/RepositoryTest.cs
+++ Tests/RepositoryTest.cs
@@ -1,6 +1,9 @@
 using FluentAssertions;
 using API.Repositories;
 using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
 using Shared.Models;
 using Xunit;
 
@@ -9,7 +12,7 @@ namespace Tests
     public class RepositoryTest : DatabaseBaseTest
     {
         private readonly HouseRepository _repository;
-        private readonly House _newProduct;
+        private House _newProduct;
 
         public RepositoryTest()
         {
@@ -46,5 +49,85 @@ namespace Tests
         {
             _repository.Invoking(r => r.CreateHouseAsync(null, null)).Should().Throw<ArgumentNullException>();
         }
+
+        [Fact]
+        public async void MakeHouse_Then_GetHouseByIdAsyncShouldReturnTheHouseDetails()
+        {
+            var house = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", _newProduct);
+
+            //Assert 
+            house.Should().BeOfType<House>();
+
+            var result = await _repository.GetHouseByIdAsync(house.Email, house.Id);
+
+            //Assert 
+            result.Should().BeOfType<House>();
+        }
+
+        [Fact]
+        public async void Given_EmptyHouse_Then_GetHouseByIdAsyncShouldBeNull()
+        {
+            var house = new House();
+
+
+            var result = await _repository.GetHouseByIdAsync(house.Email, house.Id);
+            result.Should().BeNull();
+        }    
+        [Fact]
+        public async void MakeHouse_Then_DeleteHouseAsyncShouldReturnTheHouseDetails()
+        {
+            var house = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", _newProduct);
+
+
+            var result = await _repository.DeleteHouseAsync(house.Email, house.Id);
+            result.Should().BeOfType<House>();
+        }
+        
+        [Fact]
+        public async void Given_EmptyHouse_Then_DeleteHouseAsyncShouldBeNull()
+        {
+            var house = new House();
+
+
+            var result = await _repository.GetHouseByIdAsync(house.Email, house.Id);
+            result.Should().BeNull();
+        }
+        
+        [Fact]
+        public async void Given_HouseWithWrongId_Then_DeleteHouseAsyncShouldBeNull()
+        {
+            var house = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", _newProduct);
+
+
+            var result = await _repository.GetHouseByIdAsync(house.Email, Guid.Parse("3f953890-20ad-48b5-a272-c0faa8f09ea3"));
+            result.Should().BeNull();
+        }
+        
+        
+        [Fact]
+        public async void Given_Email_Then_GetHousesAsyncShouldReturnAListOfHouses()
+        {
+            var house1 = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", new House());
+            var house2 = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", new House());
+            var house3 = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", new House());
+            var house4 = await _repository.CreateHouseAsync("gdsicadsa@popescu.com", new House());
+
+
+            var result = await _repository.GetHousesAsync(house1.Email);
+            result.Should().BeOfType<List<House>>();
+        }
+        
+        [Fact]
+        public async void Given_Email_WithNoHouses_Then_GetHousesAsyncShouldReturnAnEmptyListOfHouses()
+        {
+            var email = "empty_email@gmail.com";
+
+
+            var result = await _repository.GetHousesAsync(email);
+            result.Should().BeOfType<List<House>>();
+            result.Count().Should().Be(0);
+        }
+        
+        
     }
 }
