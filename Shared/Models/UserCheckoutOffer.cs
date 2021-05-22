d2b16baea932d0f3d954479bfa5a370dad677c49
silviuilas <silviuilas@gmail.com>
2021-05-22 14:35:04 +0300

    Ups

---
+++ Shared/Models/UserCheckoutOffer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    public class UserCheckoutOffer : BaseModel
+    {
+        [Required] public String Email { get; set; }
+        
+        [Required] public Guid CheckoutOffersId { get; set; }
+        
+        internal CheckoutOffer CheckoutOffer { get; set; }
+    }
+}
